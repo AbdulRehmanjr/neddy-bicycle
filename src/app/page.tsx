c91a95e0ba6c2ec3189112a70d23e70aec3d32c5
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2024-05-19 15:50:03 +0500

    header picture added font added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
 
   return (
     <section className="col-span-12 flex flex-col gap-14 min-h-[calc(100vh_-_124px)]">
-      <h1 className={`text-center font-extrabold text-5xl font-libre text-yellow`}  >Choose your excursion</h1>
+      <h1 className={`text-center font-extrabold text-5xl font-libre text-yellow`}>
+        Choose your excursion
+        </h1>
       <div className=" flex justify-center items-center gap-14">
         <BikeOptions />
       </div>
