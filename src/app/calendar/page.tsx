12ecd334c61563c03d430bb2665a63ffea48775d
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2024-05-29 07:44:23 +0500

    fix: paypal order not calculting well

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -10,8 +10,9 @@ export default function CalendarPage() {
             <h1 className={`text-center text-3xl md:text-5xl font-ibm text-yellow mb-6 md:mb-16`}>
                 Choose your rental period
             </h1>
-            <div className="flex flex-col items-center gap-2 text-dim-grey text-sm md:text-lg">
-                <p>7+1 days discount - rent bicycles for 7 or more days and you will get one day for free</p>
+            <div className="flex flex-col items-center gap-2 text-dim-grey text-sm md:text-lg p-4">
+                <p>7+1 days discount - rent bicycles for 7</p>
+                <p>or more days and you will get one day for free</p>
             </div>
             <div className=" flex justify-center items-center gap-14">
                 <BikeCalendar />
