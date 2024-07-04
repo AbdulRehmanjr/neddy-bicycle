876fbaf496951f4d38f3bf50f120ace65de9f653
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2024-07-04 20:22:02 +0500

    fix: page layout changes

--- src/app/booking/payment/page.tsx
+++ src/app/booking/payment/page.tsx
@@ -1,13 +1,15 @@
 import { PayPalButton } from "~/app/_components/PayPalButton";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader } from "~/components/ui/card";
 
 export default function BookingPaymentPage() {
   return (
     <section className="col-span-12 grid place-content-center min-h-[calc(100vh_-_70px)]">
+       <h1 className={`text-center text-3xl md:text-5xl font-ibm text-yellow mb-6 md:mb-16`}>
+        Book now
+      </h1>
       <Card>
         <CardHeader>
-            <CardTitle className="text-general font-gelasio">Order payment</CardTitle>
-            <CardDescription className="font-jost">Secure your payment quickly and easily by clicking the PayPal button below.</CardDescription>
+            <CardDescription className="font-libre">Secure your payment quickly and easily by clicking the PayPal button below.</CardDescription>
         </CardHeader>
         <CardContent>
           <PayPalButton />
